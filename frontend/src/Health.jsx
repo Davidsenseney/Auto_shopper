c1afa040279ddcaf05e0d64bd5609ece508a4b99
KingJaron2 <camplogan569@gmail.com>
2026-09-15 12:56:21 -0400

    Possible working page

---
+++ frontend/src/Health.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './Health.css';
+
+
+function Health() {
+    return (
+        <div className="health-container">
+            <h1> This is the Health Page</h1>
+            <p>This is a work in progress.</p>
+        </div>
+    )
+}
+
+export default Health;
