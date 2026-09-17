55af606e51c5654e7a46a09326080f18852ab3a7
Davidsenseney <daveysenseney9@gmail.com>
2026-09-17 13:07:38 -0400

    shopping app insert

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Bri from './pages/bri.jsx';
 import DashboardLayout from './components/DashboardLayout.jsx';
 import Health from './pages/health.jsx';
-import Shopping from './pages/shopping.jsx';
 import Recipes from './pages/recipes.jsx';
+import Shopping from './pages/shopping.jsx';
 
 export default function App() {
   return (
@@ -13,8 +13,8 @@ export default function App() {
         <Route path="/" element={<DashboardLayout />}>
           <Route index element={<Bri />} /> 
           <Route path="health" element={<Health />} />
-          <Route path="shopping" element={<Shopping />} />
           <Route path="recipes" element={<Recipes />} />
+          <Route path="shopping" element={<Shopping />} />
         </Route>
       </Routes>
     </Router>
